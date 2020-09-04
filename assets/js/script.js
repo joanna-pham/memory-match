1b82b1314007606c3551f64fb9fb9594e464992b
Joanna Pham <jopham90@gmail.com>
2020-09-03 17:19:45 -0700

    added flip-one-card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
+var cards = document.querySelector('#gameCards'); //selecting id gameCards (attached to main element)
 
+cards.addEventListener('click', handleClick); //event listener for a click on the main element
+
+function handleClick(event) {
+  if (event.target.className.indexOf("card-back") === -1) { //if clicked element is not ".card-back", leave the function
+    return;
+  }
+  event.target.className += ' hidden'; //adding hidden class to event.target which is div.card-back
+}
