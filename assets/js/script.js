e4ea5068890a8a72fa8e9264db51a85980758faa
Joanna Pham <jopham90@gmail.com>
2020-09-04 17:12:43 -0700

    added match two cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,43 @@ var cards = document.querySelector('#gameCards'); //selecting id gameCards (atta
 
 cards.addEventListener('click', handleClick); //event listener for a click on the main element
 
+var firstCardClicked  // store the 1st card clicked
+
+var secondCardClicked  // store the 2nd card clicked
+
+var firstCardClasses  //store the classes of card-front siblings to the card first when clicked
+
+var secondCardClasses // store the classes of the card-front siblings to the second card when clicked
+
 function handleClick(event) {
   if (event.target.className.indexOf("card-back") === -1) { //if clicked element is not ".card-back", leave the function
     return;
   }
   event.target.className += ' hidden'; //adding hidden class to event.target which is div.card-back
+
+  if (!firstCardClicked) { //not the first card clicked
+    firstCardClicked = event.target;
+    firstCardClasses = firstCardClicked.previousElementSibling.className;
+  } else {
+    secondCardClicked = event.target;
+    secondCardClasses = secondCardClicked.previousElementSibling.className;
+    cards.removeEventListener('click', handleClick)
+
+    if (firstCardClasses === secondCardClasses) {
+      // console.log('the images match')
+      firstCardClicked = null; //resetting global variables around each round
+      secondCardClicked = null;
+      cards.addEventListener('click', handleClick); //whether the cards match or not, you need to click for next round
+
+    } else {
+      setTimeout(function () { //waiting to hide again
+        firstCardClicked.classList.remove('hidden');
+        secondCardClicked.classList.remove('hidden');
+        firstCardClicked = null; //resetting global variables after each round
+        secondCardClicked = null;
+        cards.addEventListener('click', handleClick); //whether the cards match or not, you need to click for next round
+      }, 1500)
+    }
+  }
+
 }
